d1190f6800ddc5c7c554e96ee81591d64ad239d3
dkolovos <dkolovos@cs.york.ac.uk>
2017-03-15 15:26:27 +0000

    Added first unit tests

---
+++ org.eclipse.epsilon.emc.simulink.test/src/org/eclipse/epsilon/emc/simulink/test/SimulinkModelTests.java
@@ -0,0 +1,45 @@
+package org.eclipse.epsilon.emc.simulink.test;
+
+import java.io.File;
+import java.util.UUID;
+
+import org.eclipse.epsilon.emc.simulink.SimulinkModel;
+import org.eclipse.epsilon.eol.EolModule;
+import org.junit.Test;
+
+public class SimulinkModelTests {
+	
+	@Test
+	public void createGain() {
+		run("var gain = new `simulink/Math Operations/Gain`;");
+	}
+	
+	@Test
+	public void setAndGetGainGain() {
+		// gain.gain returns a Character at the moment
+		run("var gain = new `simulink/Math Operations/Gain`; gain.gain = 3; assert('3' = (gain.gain + ''));");
+	}
+	
+	protected void run(String eol) {
+		SimulinkModel model = null;
+		try {
+			EolModule module = new EolModule();
+			module.parse(eol);
+			model = new SimulinkModel();
+			model.setName("M");
+			model.setFile(new File("model" + String.valueOf(UUID.randomUUID()).replace("-", "") + ".slx"));
+			model.setReadOnLoad(false);
+			model.setStoredOnDisposal(false);
+			model.load();
+			module.getContext().getModelRepository().addModel(model);
+			module.execute();
+		}
+		catch (Exception ex) { 
+			throw new RuntimeException(ex);
+		}
+		finally {
+			 model.dispose();
+		}
+	}
+	
+}
